cfea17949d4c6b43886e6bb25b3b11b6b8100a1f
TheBlueOompaLoompa <53215039+TheBlueOompaLoompa@users.noreply.github.com>
2024-01-24 09:12:53 -0600

    Add files via upload

---
+++ devsetup.ps1
@@ -0,0 +1,23 @@
+﻿$path = "C:/Users/$Env:UserName/AppData/Local/Programs/devenv"
+$version = "v20.11.0"
+$download = "https://nodejs.org/dist/$version/node-$version-win-x64.zip"
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+if(![System.IO.File]::Exists($path + "/node-$version-win-x64.zip")) {
+	if([System.IO.File]::Exists($path + "/node.exe")) {
+		Remove-Item $path
+	}
+    mkdir $path -ea 0
+    Set-Location $path
+    Invoke-WebRequest $download -OutFile "node-$version-win-x64.zip"
+    Expand-Archive "$path/node-$version-win-x64.zip" -DestinationPath $path
+    Move-Item -Path ./node-$version-win-x64/* -Destination ./
+}
+
+
+if(!($env:Path -like "*$path*")) {
+    [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User") + ";$path","User")
+}
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
